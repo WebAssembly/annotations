7ad620cb8b9b4a47b2751206bcd3236a7d1b21dc
rossberg <rossberg@users.noreply.github.com>
2024-05-21 17:36:28 +0000

    deploy: ff86dc295b3c3aea128376fba4c756ae7767d2a3

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 + custom annotations (Draft 2024-04-10)',
+    VERSION: '2.0 + custom annotations (Draft 2024-05-21)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
